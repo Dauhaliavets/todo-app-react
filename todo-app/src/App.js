1d9e5060f3ade08f3f218df4f6d141d227ce6c9a
Dzmitry Dauhaliavets <dolgolevets21@gmail.com>
2022-03-12 21:56:58 +0300

    feat: add Footer

--- todo-app/src/App.js
+++ todo-app/src/App.js
@@ -1,12 +1,14 @@
 import './App.css';
 import Home from './Components/Home/Home';
 import Header from './Components/Header/Header';
+import Footer from './Components/Footer/Footer';
 
 function App() {
 	return (
 		<div className='App'>
 			<Header />
 			<Home title={'Todo App'}/>
+			<Footer />
 		</div>
 	);
 }
